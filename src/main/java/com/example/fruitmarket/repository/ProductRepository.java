80fa28142c5b5441ec7f9ef9b54201b894f46fe1
QuangVMSE192755 <vminhquang05@gmail.com>
2025-10-31 17:38:17 +0700

    Thêm html cho product có header và footer

--- src/main/java/com/example/fruitmarket/repository/ProductRepository.java
+++ src/main/java/com/example/fruitmarket/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.fruitmarket.repository;
 
-public interface ProductRepository {
+import com.example.fruitmarket.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
 }
