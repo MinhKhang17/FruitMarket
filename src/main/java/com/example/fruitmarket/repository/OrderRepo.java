5149a3781888adfb0cb0c356c790bef34f7c6927
TranNhatGiaHuy <trannhatgiahuyjob@gmail.com>
2025-11-03 00:01:52 +0700

    hiển thị order, update status

--- src/main/java/com/example/fruitmarket/repository/OrderRepo.java
+++ src/main/java/com/example/fruitmarket/repository/OrderRepo.java
@@ -3,11 +3,15 @@ package com.example.fruitmarket.repository;
 import com.example.fruitmarket.model.Order;
 import com.example.fruitmarket.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface OrderRepo extends JpaRepository<Order,Long> {
-    Order findOrderById(Long Id);
-    List<Order> findAllByUsersOrderByIdDesc(Users user);
-
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Long> {
+    List<Order> findAllByUsersOrderByIdDesc(Users users);
+    Order findOrderById(Long id); // Có thể trả về null
+    Optional<Order> findById(Long id); // Trả về Optional
+    List<Order> findAll();
 }
